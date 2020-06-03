f19d0c9c5f04e9bd1ba770db04fa0c0789e1fc5c
EDGARRUS <37205598+EDGARRUS@users.noreply.github.com>
2020-06-03 17:31:30 +0300

    Изменил роутинг

--- src/express/routes/categories-routes.js
+++ src/express/routes/categories-routes.js
@@ -4,6 +4,6 @@ const {Router} = require(`express`);
 
 const categoriesRouter = new Router();
 
-categoriesRouter.get(`/categories`, (req, res) => res.send(`/categories`));
+categoriesRouter.get(`/`, (req, res) => res.send(`/categories`));
 
 module.exports = categoriesRouter;
